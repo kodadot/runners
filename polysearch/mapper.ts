9d62d7546d3a27f7bbc91ae0066836ebd57c2829
Viki Val <viktorko99@gmail.com>
2023-09-25 14:27:25 +0200

    :bug: return null from mapper if name or image is not present

--- polysearch/mapper.ts
+++ polysearch/mapper.ts
@@ -2,11 +2,21 @@ import { CHAIN } from "./env.ts";
 
 // deno-lint-ignore no-explicit-any
 export const mapToCollectionInsert = (collection: any) => {
+  const content = {
+    name: collection.name,
+    image: collection.image || collection.meta?.image,
+    media_url: collection.media || collection.meta?.animation_url,
+  }
+
+  if (!content.image || !content.name) {
+    return null;
+  }
+
   return {
     id: collection.id,
-    name: collection.name,
-    image: collection.image || collection.meta.image,
-    media_url: collection.media || collection.meta.animation_url,
+    name: content.name,
+    image: content.image,
+    media_url: content.media_url,
     metadata: collection.metadata,
     chain: CHAIN,
     volume: 0,
