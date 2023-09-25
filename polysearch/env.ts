34fd6034c235915a19d40931743b34eb3ce1cda0
Viki Val <viktorko99@gmail.com>
2023-09-25 14:31:33 +0200

    :zap: min supply is 10 nfst

--- polysearch/env.ts
+++ polysearch/env.ts
@@ -1,3 +1,3 @@
 export const CHAIN = Deno.env.get("CHAIN") || "ahk";
 export const DAYS = Number(Deno.env.get("DAYS")) || 30;
-export const SUPPLY = Number(Deno.env.get("SUPPLY")) || 5;
+export const SUPPLY = Number(Deno.env.get("SUPPLY")) || 10;
