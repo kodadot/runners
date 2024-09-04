78f9f24e8558fca93a79bf5ea17d86207ed99287
Viki Val <viktorko99@gmail.com>
2024-09-04 13:09:04 +0200

    Update env.ts

--- polysearch/env.ts
+++ polysearch/env.ts
@@ -1,3 +1,3 @@
 export const CHAIN = Deno.env.get("CHAIN") || "ahk";
-export const DAYS = Number(Deno.env.get("DAYS")) || 30;
+export const DAYS = Number(Deno.env.get("DAYS")) || 120;
 export const SUPPLY = Number(Deno.env.get("SUPPLY")) || 9;
